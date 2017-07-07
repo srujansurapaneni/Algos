7a083fcfb97cc68c1292c14b49f77d0ba239f581
srujan surapaneni <srujansurapaneni@hotmail.com>
2017-07-07 00:55:51 -0400

    Create hashing.py

---
+++ hashing.py
@@ -0,0 +1,8 @@
+# /*************************************       Hashing      ************************************/
+# Problem : find the matching keys for a key of interest (keys will change once a match is found)
+# 1.for each key of interest iterate over all of the input dataset and distribute them to sparate buckets (hashes) based on a specific feature
+# 2.again iterate over each of those piles and distribute them by another feature
+# 3. repeat the iteration when no more features are left (remember, do not hash them by unique value/ primary key of the input data
+# this is called look-up and hashing. more techniques such as sorting in cobination with bucketing/hashing
+# /*********************************************************************************************/
+
