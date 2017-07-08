a7f0e1502dd6138692b220b063a303968bfef6d0
srujan surapaneni <srujansurapaneni@hotmail.com>
2017-07-07 21:09:41 -0400

    Create logarithmic_time.py

---
+++ logarithmic_time.py
@@ -0,0 +1,7 @@
+# /*************************************       logarithmic time      ************************************/
+# Problem : find the matching value from a given list of values
+# 1. Sort the dataset/list
+# 2. Find the centerpoint and compare the value of interest 
+# 3. If the value is greater or lower, move to left or right accordingly based on the sort used
+# 4. Iterate the process until you get a match
+# /*********************************************************************************************/
